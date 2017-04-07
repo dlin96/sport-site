a28158040ea2645db8a22e6caec415be0d1a7863
David Lin <davxlin@gmail.com>
2017-04-06 17:13:24 -0700

    populate database script added

--- info_retrieval.py
+++ info_retrieval.py
@@ -2,10 +2,11 @@
 # Author: David Lin 
 # Date: 3/30/17
 
-import urllib
-import json
+import urllib, json, MySQLdb
+import pprint as pp
 
-nba_base_url = 'http://api.suredbits.com/nba/v0/players/'
+# global variables
+nba_base_url = 'http://api.suredbits.com/nba/v0/players'
 
 """
 inputs: first_name, last_name
@@ -22,9 +23,73 @@ JSON object of player information (not statistics).
 E.g. height, weight, etc. 
 """
 
-def player_info_JSON(first_name, last_name):
-	url = nba_base_url + lastname + '/' + first_name
-	response = urllib.urlopen(url).read()
-	data = json.loads(response)
+
+def player_info_json(first_name, last_name):
+    url = nba_base_url + '/' + last_name + '/' + first_name
+    response = urllib.urlopen(url).read()
+    data = json.loads(response)
+    pp.pprint(data)
+
+"""
+Inputs: none
+Outputs: It will loop through the list of active players and insert them into the 
+players database. 
+
+Purpose: Populate our MySQL database with the active players in the roster. 
+Variables: 
+response - the body of the http response we get from the urllib call.
+active_roster - array of json objects representing players in the NBA. 
+"""
 
 
+def populate_nba_players():
+    # connect to MySQL db
+    db = MySQLdb.connect(host="localhost",
+                         user="root",
+                         passwd="PaloAltan01-14",
+                         db="nba")
+    # create Cursor object to execute queries
+    cur = db.cursor()
+    response = urllib.urlopen(nba_base_url).read()
+    active_roster = json.loads(response)
+    for player in active_roster:
+        player_ln = player['lastName']
+        player_fn = player['firstName']
+        player_full = player['fullName']
+        if 'height' not in player:
+            player_height = '0-0'
+        else:
+            player_height = player['height']
+        if 'weight' not in player:
+            player_weight = 0
+        else:
+            player_weight = player['weight']
+        player_profileURL = player['profileUrl']
+        player_team = player['team']
+        if 'position' not in player:
+            player_position = 'Empty'
+        else:
+            player_position = player['position']
+        player_playerId = player['playerId']
+        player_status = player['status']
+        if 'uniformNumber' not in player:
+            player_number = 0
+        else:
+            player_number = player['uniformNumber']
+        cur.execute('''INSERT INTO players (lastName, firstName, fullName, height, weight, profileURL, team, position, playerId, status, number)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''',
+                    (player_ln, player_fn, player_full, player_height, player_weight, player_profileURL, player_team, player_position, player_playerId, player_status, player_number))
+
+        db.commit()
+    db.close()
+
+'''
+Inputs: The columns of the players db (some of which will be optional).
+Purpose: Insert a specific player into the players database that might not be in the current DB. 
+'''
+# def insert_player(last_name, first_name, full_name, height, weight, profileURL, team, position, playerId, status, number):
+
+
+# if the program is being run as standalone
+if __name__ == '__main__':
+    populate_nba_players()
