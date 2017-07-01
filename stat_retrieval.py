96957bd05ef899a8367bc59a64d421d0b0027aca
David Lin <davxlin@gmail.com>
2017-07-01 11:32:31 -0700

    added stats to db

--- stat_retrieval.py
+++ stat_retrieval.py
@@ -1,26 +1,69 @@
 import urllib
 import json
-import MySQLdb
+import pymysql
 
 nba_stat_base_url = 'http://api.suredbits.com/nba/v0/stats'
 
 
 def nba_roster_stats_population():
     # connect to MySQL db
-    db = MySQLdb.connect(host="sports-db.ceutzulos0qe.us-west-1.rds.amazonaws.com",
+    db = pymysql.connect(host="sports-db.ceutzulos0qe.us-west-1.rds.amazonaws.com",
                          user="root",
                          passwd="warriors73-9",
                          db="nbadb")
     # create Cursor object to execute queries
-    cur = db.cursor()
-    cur.execute('''SELECT firstName, lastName, playerId FROM players''');
+    cur = db.cursor(pymysql.cursors.DictCursor)
+    cur.execute('''SELECT fullName FROM players ORDER BY lastName''')
+    player_list = []
 
-    row = cur.fetchone();
-    print row
-    # url = nba_stat_base_url + '/james/lebron'
-    # response = urllib.urlopen(url).read()
-    # stat_body = json.loads(response)
-    # print stat_body[0]['plusminus']
+    for i in range(0, 445):
+        row = cur.fetchone()
+        fullName = row['fullName']
+        player_list.append(fullName)
+
+    # fetch all players in the db
+    for j in range(0, 445): # TODO: don't use magic numbers here
+        name = player_list[j].split()
+        fname = name[0].lower()
+        lname = name[1].lower()
+
+        if ',' in lname:
+            temp = lname.split(',')
+            lname = temp[0]
+            
+        url = nba_stat_base_url + '/' + lname + '/' + fname
+        print url
+        response = urllib.urlopen(url).read()
+        stat_body = json.loads(response)
+        if len(stat_body) > 0:
+            stats = stat_body[0]
+            # # st, fta, bs, off, pf, min, fgm, tos, def, pts, tpa, playerId, ftm, fga, plusminus, ast, tpm, tot
+            player_st = stat_body[0]['st']
+            player_fta = stat_body[0]['fta']
+            player_bs = stat_body[0]['bs']
+            player_off = stat_body[0]['off']
+            player_pf = stat_body[0]['pf']
+            player_min = stat_body[0]['min']
+            player_fgm = stat_body[0]['fgm']
+            player_tos = stat_body[0]['to']
+            player_def = stat_body[0]['deff']
+            player_pts = stat_body[0]['pts']
+            player_tpa = stat_body[0]['tpa']
+            player_playerId = stat_body[0]['playerId']
+            player_ftm = stat_body[0]['ftm']
+            player_fga = stat_body[0]['fga']
+            player_plusminus = stat_body[0]['plusminus']
+            player_ast = stat_body[0]['ast']
+            player_tpm = stat_body[0]['tpm']
+            player_tot = stat_body[0]['tot']
+
+            cur.execute('''INSERT INTO stats (st, fta, bs, offreb, pf, min, fgm, tos, defreb, pts, tpa, playerId, ftm, fga, plusminus, ast, tpm, totreb)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''',
+                        (player_st, player_fta, player_bs, player_off, player_pf, player_min, player_fgm, player_tos, player_def, player_pts, player_tpa, player_playerId,
+                            player_ftm, player_fga, player_plusminus, player_ast, player_tpm, player_tot))
+
+            db.commit()
+    db.close()
 
 if __name__ == '__main__':
     nba_roster_stats_population()
