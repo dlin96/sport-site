0f10cb05ca8ffd37cbfdcb681f8653dd0a25d487
Alex Man <alexman@usc.edu>
2017-07-09 01:21:14 -0700

    unsure how to parse query when selecting multiple data

--- web/index.js
+++ web/index.js
@@ -57,6 +57,44 @@ app.get('/search', function(req, res) {
 	});
 });
 
+app.get('/json/:playername', function (req, res) {
+	var playername = req.params.playername;
+	// create mysql connection
+	var con = mysql.createConnection({
+		host: "sports-db.ceutzulos0qe.us-west-1.rds.amazonaws.com",
+		user: "root",
+		password: "warriors73-9",
+		database: "nbadb"
+	});
+
+	//connect to db
+	con.connect();
+	
+	//retrieve playerid
+	var playerIDquery = 'SELECT playerId FROM players WHERE fullname LIKE "%' + playername +'%"';
+	con.query(playerIDquery, function(err, rows, fields) {
+		//data should only contain one player id
+		var playerID = rows[0].playerId;
+
+		var statsQuery = 'SELECT pts, ast, to, tpm WHERE playerId LIKE "%' + playerID + '%"';
+
+		con.query(statsQuery, function(err, rows, fields) {
+			
+/*
+NOT SURE HOW TO PARSE THROUGH THIS QUERY
+*/
+
+			// var json = JSON.stringify(data);
+			// console.log(json);
+			// res.end(json);
+		});
+	});
+});
+
+function queryPlayerStats(callback) {
+
+}
+
 function queryDb(callback) {
 	// create mysql connection 
 	var con = mysql.createConnection({
